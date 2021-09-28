503831dcbeb9e6ca3d1e7858a26a33fff02bce55
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-09-28 12:01:00 -0300

    await error

--- index.mjs
+++ index.mjs
@@ -1,4 +1,14 @@
-import { readFile } from 'fs/promises'
+import { readFile, writeFile } from 'fs/promises'
 
+let template = await readFile(new URL('./template.html', import.meta.url), 'utf-8')
 
-const template = await readFile(new URL('template.html', import.meta.url))
+const data = {
+  title: 'My new file',
+  body: 'I wrote this file to disk using node'
+}
+
+for (const [key, val] of Object.entries(data)) {
+  template = template.replace(`{${key}}`, val)
+}
+
+await writeFile(new URL('./index.html', import.meta.url), template)
