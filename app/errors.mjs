b834bb72a181af082f9d03d6b2fec021b78e90a3
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-09-30 14:03:02 -0300

    errors.mjs

--- app/errors.mjs
+++ app/errors.mjs
@@ -1,8 +1,4 @@
 import {readFile} from 'fs/promises'
 
-try{
-    const result = await readFile(new URL('app.mjs', import.meta.url), 'utf-8')
-} catch (e) {
-    throw e
-    console.log('hello')
-}
+const result = await readFile(new URL('app.mjs', import.meta.url), 'utf-8')
+console.log('hello')
